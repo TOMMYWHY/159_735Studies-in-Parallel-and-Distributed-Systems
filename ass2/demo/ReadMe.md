d4f63e15d12db40b9f63912f4947a245aeb2019c
tommy <tommywhy1989@gmail.com>
2020-08-16 10:26:22 +1200

    sort

--- ass2/demo/ReadMe.md
+++ ass2/demo/ReadMe.md
@@ -1,2 +1,8 @@
  mpicc sort.c -o out
  mpirun out 10000
+ 
+#  todo
+1. report
+2. to cpp
+3. todo
+
---
+++ ass2/demo/test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tommy on 2020/8/13.
+//
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "testing"<<endl;
+    return 0;
+}
